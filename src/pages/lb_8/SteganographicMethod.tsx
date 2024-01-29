c62c5d354700d61bc95fc453b53c3ba24fcf5f9e
Gabriel <alecksandrov15@gmail.com>
2024-01-29 20:50:37 +0300

    feat: Реализованы стеганографические методы шифрования и дешифрования

--- src/pages/lb_8/SteganographicMethod.tsx
+++ src/pages/lb_8/SteganographicMethod.tsx
@@ -1,6 +1,6 @@
 import { MouseEventHandler, useState } from "react"
 import { Form, FormProps } from "../../components/Form"
-import { decToBin } from "../../utils"
+import { binToDec, decToBin } from "../../utils"
 
 function SteganographicMethod() {
 	const [form, setForm] = useState<FormProps>({
@@ -11,25 +11,62 @@ function SteganographicMethod() {
 
 	// message: string - Текст-Контейнер
 	// length: number - Количество бит шифруемого текста
-	const messageProcessing = (message: string, length: number, separator: string | Array<string>) => {
+	const messageProcessing = (message: string, input: string) => {
+		const separator = ".?!";
+		let result = "";
+		let numOfSpaces;
+		let indexToBin = 0;
+		console.log(input);
 		for (let i = 0; i < message.length; i++) {
-			if (separator.includes(message[i])) {
-				let numOfSpaces = 0;
+			numOfSpaces = 0;
+
+			if (input[indexToBin] === null || input[indexToBin] === undefined) {
+				result += "^"
+				result += message.slice(i);
+				return result;
+			}
+
+			while (separator.includes(message[i])) {
+				result += message[i];
 				i++;
+			}
 
-				while(message[i] == " ") {
-					if (message[i] === undefined) break;
-					numOfSpaces++;
-					i++;
-				} 
+			while(message[i] === " ") {
+				numOfSpaces++;
+				i++;
+			}
 
-				console.log("numOfSpaces", numOfSpaces);
-				// Проверить наличие пробела
-				// Удалить лишние пробелы
-				// Добавить пробел если кодируется 0
-				// Оставить один пробел если кодируется 1
+			if (numOfSpaces > 0) {
+				i--;
+				if (input[indexToBin] === "0") {
+					result += "  ";
+				} else if (input[indexToBin] === "1") {
+					result += " ";
+				}
+				indexToBin++;
+			} else {
+				result += message[i];
 			}
 		}
+		if (input[indexToBin] !== null || input[indexToBin] !== undefined) return "Текст-контейнер недостаточной длины для шифрования";
+	}
+
+	const decrypt = (input: string) => {
+		let result = "";
+		let numOfSpaces;
+		for (let i = 0; i < input.length; i++) {
+			if (input[i] === "^") break;
+			numOfSpaces = 0;
+			while(input[i] === " ") {
+				numOfSpaces++;
+				i++;
+			}
+			if (numOfSpaces > 0) {
+				result += numOfSpaces > 1 ? "0": "1";
+				i--;
+			}
+		}
+		return binToDec(result, 8);
 	}
 
 	const encryptHandler: MouseEventHandler<HTMLButtonElement> = (event) => {
@@ -37,14 +74,18 @@ function SteganographicMethod() {
 		const key = form.key;
 		const input = form.input;
 		if (input && key) {
-			const binInput: Array<string> = decToBin(input, 8);
-			messageProcessing(key, binInput.length * binInput[0].length, ".?!");
+			const binInput: string = decToBin(input, 8);
+			const result =messageProcessing(key, binInput);
+			setForm((prev) => ({ ...prev, textArea: result }))
 		}
 	}
 
 	const decryptHandler: MouseEventHandler<HTMLButtonElement> = (event) => {
 		event.preventDefault();
 		
+		const result = decrypt(form.input);
+		console.log(result);
+		setForm((prev) => ({ ...prev, textArea: result }));
 	}
 
 	return <Form form={form} setForm={setForm} encrypt={encryptHandler} decrypt={decryptHandler}></Form>
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
-export const decToBin = (arr: string, size: number): Array<string> => {
-	return arr.split("").map(item => item.charCodeAt(0)?.toString(2).padStart(size, "0"));
+export const decToBin = (arr: string, size: number): string => {
+	return arr.split("").map(item => item.charCodeAt(0)?.toString(2).padStart(size, "0")).join("");
+}
+
+export const binToDec = (arr: string, size: number): string => {
+	const result = [];
+	for (let i = 0; i < arr.length / size; i++) {
+		const block = arr.slice(i * size, i * size + size);
+		const char = parseInt(block, 2);
+		result.push(String.fromCharCode(char));
+	}
+	return result.join("");
 }
